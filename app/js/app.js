285360c71fd4b8928f8832da71052c3f79b90c9d
Your Name <godfredakpan@gmail.com>
2018-04-11 21:23:14 +0100

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var mailchimpSubscriptionApp = angular.module('mailchimpSubscriptionApp', [
+  'ngRoute',
+  'ngAnimate',
+  'ngResource',
+  'ngSanitize',
+  'mailchimpSubscriptionControllers'
+]);
+
